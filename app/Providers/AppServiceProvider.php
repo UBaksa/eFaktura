21ac251d75755e011ac25d358dc96e27643969aa
Ujkanovic Bakir <ujkanovicbakir03@gmail.com>
2026-06-07 19:17:06 +0200

    fixx mailllllklll

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,12 @@
 
 namespace App\Providers;
 
-use App\Mail\Transport\MailtrapTransport;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Mail::extend('mailtrap', function () {
-            return new MailtrapTransport(env('MAILTRAP_API_TOKEN'));
-        });
+        //
     }
 }
